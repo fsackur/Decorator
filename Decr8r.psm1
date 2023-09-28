b00804d83e2dbeaf8716f5b66e25972587990c19
Freddie Sackur <github@dustyfox.uk>
2023-09-28 11:55:47 +0100

    Banging head on applying decorators with normal function calls

--- Decr8r.psm1
+++ Decr8r.psm1
@@ -18,19 +18,18 @@ class DecoratedCommand
 
 class DecorateWithAttribute : Attribute
 {
-    DecorateWithAttribute ([Management.Automation.CommandInfo]$Decorator)
-    {
-        $this.Decorator = $Decorator
-        [void][DecoratedCommand]::_decoratedCommands.Add($this.Decorator)
-    }
-
     DecorateWithAttribute ([string]$DecoratorName)
     {
-        $this.Decorator = Get-Command $DecoratorName
-        [void][DecoratedCommand]::_decoratedCommands.Add($this.Decorator)
+        Write-Host "In DecorateWithAttribute" -ForegroundColor DarkGray
+        # Get-PSCallStack | ft | os | write-host -ForegroundColor DarkGray
+        # $this.Decorator = Get-Command $DecoratorName
+        # $this.Decorator | ft | os | write-host -ForegroundColor DarkGray
+        # [DecoratedCommand]::_decoratedCommand = $this.Decorator
+        $this.$DecoratorName = $DecoratorName
     }
 
     [Management.Automation.CommandInfo]$Decorator
+    [string]$DecoratorName
 }
 
 
@@ -58,18 +57,71 @@ function Initialize-Decorator
     # $SessionState.InvokeCommand.GetCommands('*', 'All', $true) | select -First 10 | ft | os | write-host
     # Get-Command -Module
     # $SessionState.Module | select -First 10 | ft | os | write-host
-    $SessionState.InvokeCommand.PostCommandLookupAction = {
-        param ($CommandName, $LookupEventArgs)
+    # $SessionState.InvokeCommand.PostCommandLookupAction = {
+    #     param ($CommandName, $LookupEventArgs)
 
-        $Command = $LookupEventArgs.Command
-        # if ([DecoratedCommand]::_decoratedCommands.Contains($Command))
-        $Attr = $Command.ScriptBlock.Attributes.Where({$_.TypeId -eq [DecorateWithAttribute]})
-        if ($Attr)
-        {
-            Write-Host "In PostCommandLookupAction" -ForegroundColor DarkGray
-            [DecoratedCommand]::_decoratedCommand = $Command
-            # $Attr = $Command.ScriptBlock.Attributes.Where({$_.TypeId -eq [DecorateWithAttribute]})[0]
-            $LookupEventArgs.Command = $Attr.Decorator
-        }
+    #     $Command = $LookupEventArgs.Command
+    #     # if ([DecoratedCommand]::_decoratedCommands.Contains($Command))
+    #     $Attr = $Command.ScriptBlock.Attributes.Where({$_.TypeId -eq [DecorateWithAttribute]})
+    #     if ($Attr)
+    #     {
+    #         Write-Host "In PostCommandLookupAction" -ForegroundColor DarkGray
+    #         [DecoratedCommand]::_decoratedCommand = $Command
+    #         # $Attr = $Command.ScriptBlock.Attributes.Where({$_.TypeId -eq [DecorateWithAttribute]})[0]
+    #         $LookupEventArgs.Command = $Attr.Decorator
+    #     }
+    # }
+    Write-Host "In Initialize-Decorator" -ForegroundColor DarkGray
+    Get-Command -Module $SessionState.Module | ft | os | write-host     # if we call from the .psm1, the functions haven't been exported yet and this returns $null
+
+    # So, use reflection to get the internal table
+    $iss = [Management.Automation.SessionState].GetField('_sessionState', ([Reflection.BindingFlags]'Nonpublic, Instance')).GetValue($SessionState)
+    $FunctionTable = $iss.GetType().GetMethod('GetFunctionTable', ([Reflection.BindingFlags]'Nonpublic, Instance')).Invoke($iss, @())
+
+    $ModuleFunctions = $FunctionTable.Values.Where({$_.Module -eq $SessionState.Module})
+    $DecoratedFunctions = $ModuleFunctions.Where({$_.ScriptBlock.Attributes.Where({$_.TypeId -eq [DecorateWithAttribute]})})
+
+    $DecoratedFunctions | ft | os | write-host
+    $DecoratedFunctions | Update-Function
+}
+
+function Update-Function
+{
+    [OutputType([void])]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [Alias('InputObject')]
+        [Alias('Command')]
+        [Management.Automation.FunctionInfo]$DecoratedFunction
+    )
+
+    begin
+    {
+        $UpdateMethod = [Management.Automation.FunctionInfo].GetMethod(
+            'Update',
+            ([Reflection.BindingFlags]'Nonpublic, Instance'),
+            [type[]]([scriptblock], [bool], [Management.Automation.ScopedItemOptions], [string])
+        )
+    }
+
+    process
+    {
+        $DecoratorAttribute = $DecoratedFunction.ScriptBlock.Attributes.Where({$_.TypeId -eq [DecorateWithAttribute]})
+        $Decorator = $DecoratorAttribute.Decorator
+        $Wrapper = {
+            [DecoratedCommand]::_decoratedCommand = $DecoratedFunction
+            & $Decorator @args
+        }.GetNewClosure()
+        $UpdateMethod.Invoke(
+            $DecoratedFunction,
+            (
+                $Wrapper,
+                $Force,
+                $DecoratedFunction.Options,
+                ([string]$DecoratedFunction.HelpFile)
+            )
+        )
     }
 }
