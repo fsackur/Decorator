f5859c924daac5074342a1bbc035aea32843222e
Freddie Sackur <github@dustyfox.uk>
2023-10-05 17:55:58 +0100

    Workaround using StaticParameterBinder

--- src/DecorateCommand.cs
+++ src/DecorateCommand.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Reflection;
 using System.Management.Automation;
+using System.Management.Automation.Language;
 using SMA = System.Management.Automation;
 using System.Diagnostics;
 using System.Linq;
 using System.Collections;
+using ObjectModel = System.Collections.ObjectModel;
 using System.Collections.Generic;
 
 namespace Decr8r
@@ -18,13 +20,28 @@ namespace Decr8r
 
         static DecoratedCommand()
         {
+            commandParam = new(
+                "Command",
+                typeof(CommandInfo),
+                new ObjectModel.Collection<Attribute>
+                {
+                    new ParameterAttribute {
+                        Position = 0,
+                        Mandatory = true
+                    },
+                    new ValidateNotNullOrEmptyAttribute()
+                }
+            );
+
             StaticParams = new HashSet<string>(CommonParameters, StringComparer.OrdinalIgnoreCase);
             StaticParams.UnionWith(OptionalCommonParameters);
-            StaticParams.Add(nameof(Command));
+            StaticParams.Add(commandParam.Name);
         }
 
-        [Parameter(Mandatory = true, Position = 0)]
+        // Not declared as Parameter as workaround for https://github.com/PowerShell/PowerShell/issues/3984
         public CommandInfo Command { get; set; }
+
+        private static readonly RuntimeDefinedParameter commandParam;
 
         private SteppablePipeline? _pipeline;
 
@@ -40,12 +57,119 @@ namespace Decr8r
 
         private readonly ISet<string> staticBoundParams = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
+        private object context { get => Reflected.GetPropertyValue(this, "Context")!; }
+
+        private IEnumerable GetUnboundArguments()
+        {
+            var processor = Reflected.GetPropertyValue(context, "CurrentCommandProcessor")!;
+            var parameterBinder = Reflected.GetPropertyValue(processor, "CmdletParameterBinderController")!;
+
+            // Expected runtime type: ObjectModel.Collection<CommandParameterInternal>
+            var args = Reflected.GetPropertyValue(parameterBinder, "UnboundArguments") as IEnumerable;
+            return args ?? Enumerable.Empty<object>();
+        }
+
+        // https://stackoverflow.com/a/29680516/6274530
+        // fails on abbreviated commands :-(
+        private object? GetUnboundValue(string desiredParameter, int position = -1) {
+
+            var args = GetUnboundArguments();
+
+            var currentParameterName = string.Empty;
+            object? unnamedValue = null;
+            int currentPosition = 0;
+            foreach (var arg in args) {
+
+                // Is it a param name:
+                var isParameterName = (Boolean)(Reflected.GetPropertyValue(arg, "ParameterNameSpecified") ?? false);
+                if (isParameterName)
+                {
+                    string? parameterName = Reflected.GetPropertyValue(arg, "ParameterName") as string;
+                    currentParameterName = parameterName ?? currentParameterName;
+                    continue;
+                }
+
+                // Treat as a value:
+                var parameterValue = Reflected.GetPropertyValue(arg, "ArgumentValue");
+
+                if (!string.IsNullOrEmpty(currentParameterName))
+                {
+                    // Found currentParameterName's value. If it matches paramName, return it
+                    if (currentParameterName.Equals(desiredParameter, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return parameterValue;
+                    }
+                }
+                else if (currentPosition++ == position)
+                {
+                    unnamedValue = parameterValue;  // Save this for later in case desiredParameter isn't found
+                }
+
+                // Found a value, so currentParameterName needs to be cleared
+                currentParameterName = string.Empty;
+            }
+
+            return unnamedValue;
+        }
+
+        public CallStackFrame GetCaller()
+        {
+            var Debugger = (Reflected.GetPropertyValue(context, "Debugger") as SMA.Debugger)!;
+            return Debugger.GetCallStack().Where((f) => f.FunctionName != this.GetType().Name).First()
+                ?? throw new InvalidOperationException("Could not identify calling frame");
+        }
+
+        public CallStackFrame GetMyFrame()
+        {
+            var Debugger = (Reflected.GetPropertyValue(context, "Debugger") as SMA.Debugger)!;
+            return Debugger.GetCallStack().First();
+        }
+
         public object GetDynamicParameters()
         {
             var dynParams = new RuntimeDefinedParameterDictionary();
 
+            var caller = GetCaller();
+
+            var myFrame = GetMyFrame();
+            var myCommandLine = myFrame.Position.Text;
+            Token[] tokens = null!;
+            ParseError[] errors = null!;
+            var ast = Parser.ParseInput(myCommandLine, out tokens, out errors)
+                ?? throw new ParseException(errors);
+            var statementAst = ast.EndBlock.Statements.Last() as PipelineAst
+                ?? throw new ParseException("Invocation was not a PipelineAst");
+            CommandAst commandAst = null!;
+            foreach (var a in statementAst.PipelineElements.Where((a) => a is CommandAst))
+            {
+                CommandAst c = (CommandAst)a!;
+                var command = InvokeCommand.GetCommand(c.GetCommandName(), CommandTypes.All);
+                while (command is AliasInfo)
+                {
+                    command = ((AliasInfo)command).ResolvedCommand;
+                }
+                if (command is CmdletInfo && command.Name == $"Decorate-Command")
+                {
+                    commandAst = c;
+                    break;
+                }
+            }
+                // ?? throw new ParseException("Invocation was not a CommandAst");
+
+            var bindingResult = StaticParameterBinder.BindCommand(commandAst, true);
+
+
+
             if (Command is null)
             {
+                // var context = Reflected.ContextProperty.GetValue(this);
+                // var processor = Reflected.CurrentCommandProcessorProperty.GetValue(this);
+                // var parameterBinder = Reflected.CmdletParameterBinderControllerProperty.GetValue(processor);
+                // var args = Reflected.UnboundArgumentsProperty.GetValue(parameterBinder) as System.Collections.IEnumerable;
+
+                var arg = GetUnboundValue("Command");
+                // args is empty when completing command name
+                dynParams[commandParam.Name] = commandParam;
                 return dynParams;
             }
 
@@ -64,7 +188,7 @@ namespace Decr8r
         {
             staticBoundParams.UnionWith(MyInvocation.BoundParameters.Keys);
 
-            MyInvocation.BoundParameters.Remove(nameof(Command));
+            MyInvocation.BoundParameters.Remove("Command");
 
             var ps = PowerShell.Create(RunspaceMode.CurrentRunspace);
             ps.AddCommand(Command).AddParameters(MyInvocation.BoundParameters);
--- src/Reflection.cs
+++ src/Reflection.cs
@@ -6,21 +6,67 @@ namespace Decr8r
 {
     public partial class DecoratedCommand
     {
-        internal static class ReflectedMembers
+        internal static class Reflected
         {
-            internal static BindingFlags PrivateFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            private static BindingFlags privateFlags = BindingFlags.NonPublic | BindingFlags.Instance;
 
-            internal static MethodInfo UpdateMethod = typeof(FunctionInfo).GetMethod("Update", PrivateFlags, null, new Type[] { typeof(ScriptBlock), typeof(bool), typeof(ScopedItemOptions), typeof(string) }, null)!;
+            // internal static MethodInfo UpdateMethod = typeof(FunctionInfo).GetMethod("Update", privateFlags, null, new Type[] { typeof(ScriptBlock), typeof(bool), typeof(ScopedItemOptions), typeof(string) }, null)!;
 
-            internal static PropertyInfo InternalSessionStateProperty = typeof(SessionState).GetProperty("Internal", PrivateFlags)!;
+            // internal static PropertyInfo InternalSessionStateProperty = typeof(SessionState).GetProperty("Internal", privateFlags)!;
 
-            internal static MethodInfo GetFunctionTableMethod = InternalSessionStateProperty.PropertyType.GetMethod("GetFunctionTableAtScope", PrivateFlags)!;
+            // internal static MethodInfo GetFunctionTableMethod = InternalSessionStateProperty.PropertyType.GetMethod("GetFunctionTableAtScope", privateFlags)!;
 
-            internal static PropertyInfo ContextProperty = typeof(PSCmdlet).GetProperty("Context", PrivateFlags)!;
+            // internal static PropertyInfo ContextProperty = typeof(PSCmdlet).GetProperty("Context", privateFlags)!;
 
-            internal static PropertyInfo DebuggerProperty = ContextProperty.PropertyType.GetProperty("Debugger", PrivateFlags)!;
+            // internal static PropertyInfo CurrentCommandProcessorProperty = ContextProperty.PropertyType.GetProperty("CurrentCommandProcessor", privateFlags)!;
 
-            internal static ConstructorInfo ScriptInfoCtor = typeof(ScriptInfo).GetConstructor(PrivateFlags, null, new Type[] { typeof(string), typeof(ScriptBlock), ContextProperty.PropertyType }, null)!;
+            // internal static PropertyInfo CmdletParameterBinderControllerProperty = CurrentCommandProcessorProperty.PropertyType.GetProperty("CmdletParameterBinderController", privateFlags)!;
+
+            // internal static PropertyInfo UnboundArgumentsProperty = CmdletParameterBinderControllerProperty.PropertyType.GetProperty("UnboundArguments", privateFlags)!;
+
+            // internal static PropertyInfo DebuggerProperty = ContextProperty.PropertyType.GetProperty("Debugger", privateFlags)!;
+
+            // internal static ConstructorInfo ScriptInfoCtor = typeof(ScriptInfo).GetConstructor(privateFlags, null, new Type[] { typeof(string), typeof(ScriptBlock), ContextProperty.PropertyType }, null)!;
+
+            internal static object? GetPropertyValue(object instance, string propertyName) {
+                // any access of a null object returns null.
+                if (instance == null || string.IsNullOrEmpty(propertyName)) {
+                    return null;
+                }
+
+                var propertyInfo = instance.GetType().GetProperty(propertyName, privateFlags);
+
+                if (propertyInfo != null)
+                {
+                    try
+                    {
+                        return propertyInfo.GetValue(instance);
+                    }
+                    catch {}
+                }
+
+                return null;
+            }
+
+            internal static object? InvokeMethod(object instance, string methodName, object[]? parameters) {
+                // any access of a null object returns null.
+                if (instance == null || string.IsNullOrEmpty(methodName)) {
+                    return null;
+                }
+
+                var methodInfo = instance.GetType().GetMethod(methodName, privateFlags);
+
+                if (methodInfo != null)
+                {
+                    try
+                    {
+                        return methodInfo.Invoke(instance, parameters);
+                    }
+                    catch {}
+                }
+
+                return null;
+            }
         }
     }
 }
