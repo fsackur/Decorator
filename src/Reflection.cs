f7f3b02977dd8e72b44fe87cdaeada9f7490ab2e
Freddie Sackur <github@dustyfox.uk>
2023-12-02 20:46:29 +0000

    Delete ad-hoc testing crap

--- src/Reflection.cs
+++ src/Reflection.cs
@@ -10,24 +10,6 @@ namespace Decr8r
         {
             private static BindingFlags privateFlags = BindingFlags.NonPublic | BindingFlags.Instance;
 
-            // internal static MethodInfo UpdateMethod = typeof(FunctionInfo).GetMethod("Update", privateFlags, null, new Type[] { typeof(ScriptBlock), typeof(bool), typeof(ScopedItemOptions), typeof(string) }, null)!;
-
-            // internal static PropertyInfo InternalSessionStateProperty = typeof(SessionState).GetProperty("Internal", privateFlags)!;
-
-            // internal static MethodInfo GetFunctionTableMethod = InternalSessionStateProperty.PropertyType.GetMethod("GetFunctionTableAtScope", privateFlags)!;
-
-            // internal static PropertyInfo ContextProperty = typeof(PSCmdlet).GetProperty("Context", privateFlags)!;
-
-            // internal static PropertyInfo CurrentCommandProcessorProperty = ContextProperty.PropertyType.GetProperty("CurrentCommandProcessor", privateFlags)!;
-
-            // internal static PropertyInfo CmdletParameterBinderControllerProperty = CurrentCommandProcessorProperty.PropertyType.GetProperty("CmdletParameterBinderController", privateFlags)!;
-
-            // internal static PropertyInfo UnboundArgumentsProperty = CmdletParameterBinderControllerProperty.PropertyType.GetProperty("UnboundArguments", privateFlags)!;
-
-            // internal static PropertyInfo DebuggerProperty = ContextProperty.PropertyType.GetProperty("Debugger", privateFlags)!;
-
-            // internal static ConstructorInfo ScriptInfoCtor = typeof(ScriptInfo).GetConstructor(privateFlags, null, new Type[] { typeof(string), typeof(ScriptBlock), ContextProperty.PropertyType }, null)!;
-
             internal static object? GetPropertyValue(object instance, string propertyName) {
                 // any access of a null object returns null.
                 if (instance == null || string.IsNullOrEmpty(propertyName)) {
