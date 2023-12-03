f8719fd9a22bbdaeacdc6be408d11feb1b71c304
Freddie Sackur <github@dustyfox.uk>
2023-12-03 00:08:32 +0000

    Assertion helper that skips over irrelevant lines

--- tests/Decr8r.Tests.ps1
+++ tests/Decr8r.Tests.ps1
@@ -6,17 +6,15 @@ BeforeAll {
 }
 
 Describe 'Decr8r' {
-    BeforeEach {
-        $HostWrites = [System.Collections.Generic.List[string]]::new()
-        Mock Write-Host {$HostWrites.Add([string]$Object)}
-    }
+    BeforeEach {. $WriteHostSetup}
 
     It 'Decorates a simple function' {
         . $DecoratorDeclaration
         . $SimpleFunctionDeclaration
+
         & SUT
 
-        $HostWrites | Should -Be (
+        ShouldWrite (
             "Decorator begin",
             "Decorator process",
             "Decorated end",
